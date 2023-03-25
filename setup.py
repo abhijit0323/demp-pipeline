d8d3800d3557f7092f98014248104e2148691a35
Abhijit <abhijit0382@gmail.com>
2023-03-25 18:15:22 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup,find_packages
+
+setup(name="census-income",version="0.0.1",author="abhi",author_email="abhijit0382@gmail.com",package=find_packages(),install_requirements=["pandas","numpy","flask"])
